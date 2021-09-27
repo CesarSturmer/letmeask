78626abf840803e1ffa641bd6dfbcb2a49e42736
Cesar Eduardo Sturmer <cesar_dudutm@hotmail.com>
2021-09-27 20:42:08 -0300

    adjust files

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,18 +4,17 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyDE-npB2NewsumAMXgdJFlAwDZkuLi_7_w",
+  authDomain: "page-7f29d.firebaseapp.com",
+  projectId: "page-7f29d",
+  storageBucket: "page-7f29d.appspot.com",
+  messagingSenderId: "109448461231",
+  appId: "1:109448461231:web:bf6be8d62f01f094670d5a",
+  databaseURL: "https://page-7f29d-default-rtdb.firebaseio.com/",
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
 const database = firebase.database();
 
-export { auth, database, firebase}
+export { database, firebase }
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'light',
+  colors: {
+    bodyColor: '#fff',
+    background: '#F8F8F8;',
+    grayLine: '#DCDDE0',
+    text: '#666666',
+    textHighlight: '#B3B9FF',
+    title: '#2E384D',
+    invertWhite: 'brightness(0) invert(1)',
+    invertBlack: 'brightness(10%) contrast(100%)',
+    overlay: 'rgba(254, 254, 254, 0.75)',
+  }
+}
